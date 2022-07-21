405771814c432e9196a81c8188841217e1d64d4a
Tamera Brown <Tam@Tameras-MBP.fios-router.home>
2022-07-21 19:37:54 -0400

    Dockerizing spring boot app

--- Bank/src/main/java/com/java/Bank/BankApplication.java
+++ Bank/src/main/java/com/java/Bank/BankApplication.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 
 @SpringBootApplication
-public class BankApplication implements CommandLineRunner {
+public class BankApplication {
 
 	@Autowired
 	private UserRepo userRepo;
@@ -20,25 +20,5 @@ public class BankApplication implements CommandLineRunner {
 		SpringApplication.run(BankApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		User c1 = new User();
-		c1.setFirstName("Tamera");
-		c1.setLastName("Brown");
-		c1.setEmail("tamerabrown642@gmail.com");
-		c1.setAddress("101 Water Street Riverdaile, MD 20098");
-		c1.setPhoneNum("2403748638");
 
-		userRepo.save(c1);
-
-
-		System.out.println("************************");
-
-		List<User> users = userRepo.findAll();
-
-		for (User c : users) {
-			System.out.println(c.toString());
-		}
-
-	}
 }
