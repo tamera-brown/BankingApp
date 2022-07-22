be710962b46dc93dcba9f6b3453480028416ffa9
Tamera Brown <Tam@Tameras-MBP.fios-router.home>
2022-07-21 20:36:21 -0400

    connected to mongodb on docker

--- Bank/src/main/java/com/java/Bank/BankApplication.java
+++ Bank/src/main/java/com/java/Bank/BankApplication.java
@@ -1,20 +1,11 @@
 package com.java.Bank;
-
-import com.java.Bank.model.User;
-import com.java.Bank.repo.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
 
 
 @SpringBootApplication
 public class BankApplication {
-
-	@Autowired
-	private UserRepo userRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
