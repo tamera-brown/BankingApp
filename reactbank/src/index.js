82c265ff5d7a843fff686eb8dfd3b179644e766a
Tamera Brown <Tam@Tameras-MBP.fios-router.home>
2022-08-19 22:25:25 -0400

    Added react redux

--- reactbank/src/index.js
+++ reactbank/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import { SnackbarProvider } from "notistack";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <SnackbarProvider maxSnack={3}>
+    <App></App>
+    </SnackbarProvider>
+    </Provider>, document.getElementById("root"));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ reactbank/src/reducers/reducers.jsx
@@ -0,0 +1,25 @@
+const initialState={
+    currentUser:null,
+    isAuthenticated:false
+}
+
+export default function appReducer(state=initialState,action){
+    switch(action.type){
+        case "USER_SIGN_IN":
+            return{
+                ...state,
+                currentUser:action.currentUser,
+                isAuthenticated:true
+            }
+        case "USER_SIGN_OUT":
+            return{
+                ...state,
+                currentUser:null,
+                isAuthenticated:false
+            }
+            default:
+       
+                return state
+            }
+    
+}
